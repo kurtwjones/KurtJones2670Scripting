566e56353bd633d184e2d87c2c7b20776ad3d097
kurt jones <kurtwilliamjones@gmail.com>
2016-09-17 21:20:02 -0600

    conventionsSyntax Assignment
    conventionsSyntax Assignment

---
+++ CS2670Assignments/ConventionsSyntaxAssignment.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConventionsSyntaxAssignment : MonoBehaviour {
+    public int health = 100;
+    // Use this for initialization
+    void Start () {
+     
+        //create a message to show to user
+        Debug.Log("coding is cool.");
+        Debug.Log("my health is" + health);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+         
+}
+}
+/* There is a certain way that you need to code in order for the computer
+ * to recognize what someone is trying to create with their code. Syntax
+ * is the structure of the coding language and conventions are different
+ * ways we can access different items in a compound item in the code. Semi-colons
+ * need to be placed at the end of your statements to tell the computer "this is
+ * the end of this action" comments are a great way to show others what is happening
+ * or even to remind yourself of what something in your code it doing. You can do a single
+ * line comment with a double slash or a multi line comment with the slash and astarisk at the
+ * beginning and reversing it to end the comment. You can also use it to disable code just in case
+ * it doesnt work.
+ * 
+ */
