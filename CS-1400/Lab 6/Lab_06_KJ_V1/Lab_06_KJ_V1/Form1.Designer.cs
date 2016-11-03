570d99f0bed6a19b0263c55bfdf5db1d7991a718
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 17:56:23 -0600

    lab6

---
+++ CS-1400/Lab 6/Lab_06_KJ_V1/Lab_06_KJ_V1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Lab_06_KJ_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputDiamater = new System.Windows.Forms.TextBox();
+            this.outputTurnsPerMile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(576, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(51, 29);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(147, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(147, 30);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Turns Per Mile:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Diameter of Wheel:";
+            // 
+            // inputDiamater
+            // 
+            this.inputDiamater.Location = new System.Drawing.Point(340, 120);
+            this.inputDiamater.Name = "inputDiamater";
+            this.inputDiamater.Size = new System.Drawing.Size(158, 26);
+            this.inputDiamater.TabIndex = 4;
+            this.inputDiamater.TextChanged += new System.EventHandler(this.inputDiamater_TextChanged);
+            this.inputDiamater.Leave += new System.EventHandler(this.inputDiamater_Leave);
+            // 
+            // outputTurnsPerMile
+            // 
+            this.outputTurnsPerMile.Location = new System.Drawing.Point(340, 192);
+            this.outputTurnsPerMile.Name = "outputTurnsPerMile";
+            this.outputTurnsPerMile.ReadOnly = true;
+            this.outputTurnsPerMile.Size = new System.Drawing.Size(158, 26);
+            this.outputTurnsPerMile.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(443, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pioneer Measure System";
+            // 
+            // clearButton
+            // 
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearButton.Location = new System.Drawing.Point(350, 264);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(99, 41);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab_06_KJ_V1.Properties.Resources.wagonwheel1;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 206);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 370);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputTurnsPerMile);
+            this.Controls.Add(this.inputDiamater);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Cs1400 Lab #6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputDiamater;
+        private System.Windows.Forms.TextBox outputTurnsPerMile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CS-1400/Lab 6/Lab_06_KJ_V1/Lab_06_KJ_V1/Form1.cs
@@ -0,0 +1,87 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Lab 6
+//Date: 09/02/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_06_KJ_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); //Start GUI
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // The aboutToolStripMenuItem_Click Method
+        // Purpose: Show information about the creator
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kurt Jones\n CS1400_01\n Lab #6");
+        }
+        // the clearButton_Click method
+        //Purpose : To Clear the text boxes of all information
+        //Returns : None
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            inputDiamater.Clear();
+            outputTurnsPerMile.Clear();
+        }
+        // The exitToolStripMenuItem1 method
+        // Purpose: To close the window and terminate the application
+        // Returns: None
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        // Purpose: Calculate turns per mile and output a string into the output textbox
+        private void inputDiamater_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void inputDiamater_Leave(object sender, EventArgs e)
+        {
+            // Create Constant for PI (3.14)
+            const double PI = 3.14;
+            // create Constant for inchesPerMile (63,360)
+            const double inchesPerMile = 63360;
+            // Get string from inputDiamater box
+            double input = double.Parse(inputDiamater.Text);
+            // Convert string to Double
+            // Take Double and times by PI and call it Circumference
+            double circumference = input * PI;
+            // Divide inchesPerMile to Circumferance
+            double newCircumference = inchesPerMile / circumference;
+            // output revolutionsPerMile
+            string outStr = $"{newCircumference}";
+            outputTurnsPerMile.Text = outStr;
+            
+        }
+    }
+}
+// Question 1: 75.36 inches 
+// question 2: circ = diam * PI;
+//              const PI = 3.14
