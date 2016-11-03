93f4d051f8cc3f403e160e047f26b2e6856cef78
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 17:39:24 -0600

    added assignments
    cs1400

---
+++ CS-1400/Lab 2/Cs1400Lab02KurtJones/Cs1400Lab02KurtJones/Program.cs
@@ -0,0 +1,37 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Lab 2
+//Date: 08/30/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cs1400Lab02KurtJones
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+// question 1 (D)
+// question 2 (A)
