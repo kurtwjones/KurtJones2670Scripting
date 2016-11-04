51141d3a33e6bb82bcdc3c5a93295f31db89b72c
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 18:15:37 -0600

    project1

---
+++ CS-1400/Project 1/Project_01_KJ_V1/Project_01_KJ_V1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Project_01_KJ_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.costOfMealBox = new System.Windows.Forms.TextBox();
+            this.poorServiceBox = new System.Windows.Forms.TextBox();
+            this.goodServiceBox = new System.Windows.Forms.TextBox();
+            this.excellentServiceBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(453, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(51, 29);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(147, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(147, 30);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // costOfMealBox
+            // 
+            this.costOfMealBox.Location = new System.Drawing.Point(231, 71);
+            this.costOfMealBox.Name = "costOfMealBox";
+            this.costOfMealBox.Size = new System.Drawing.Size(141, 26);
+            this.costOfMealBox.TabIndex = 1;
+            this.costOfMealBox.Leave += new System.EventHandler(this.costOfMealBox_Leave);
+            // 
+            // poorServiceBox
+            // 
+            this.poorServiceBox.Location = new System.Drawing.Point(231, 150);
+            this.poorServiceBox.Name = "poorServiceBox";
+            this.poorServiceBox.Size = new System.Drawing.Size(141, 26);
+            this.poorServiceBox.TabIndex = 2;
+            // 
+            // goodServiceBox
+            // 
+            this.goodServiceBox.Location = new System.Drawing.Point(231, 193);
+            this.goodServiceBox.Name = "goodServiceBox";
+            this.goodServiceBox.Size = new System.Drawing.Size(141, 26);
+            this.goodServiceBox.TabIndex = 3;
+            // 
+            // excellentServiceBox
+            // 
+            this.excellentServiceBox.Location = new System.Drawing.Point(231, 242);
+            this.excellentServiceBox.Name = "excellentServiceBox";
+            this.excellentServiceBox.Size = new System.Drawing.Size(141, 26);
+            this.excellentServiceBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cost Of Meal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tip For Poor Service:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tip For Good Sevice:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tip For Excellent Service:";
+            // 
+            // clearButton
+            // 
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.clearButton.Location = new System.Drawing.Point(28, 302);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(122, 37);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 406);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.excellentServiceBox);
+            this.Controls.Add(this.goodServiceBox);
+            this.Controls.Add(this.poorServiceBox);
+            this.Controls.Add(this.costOfMealBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tip Calculator_Project 1 KJ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.TextBox costOfMealBox;
+        private System.Windows.Forms.TextBox poorServiceBox;
+        private System.Windows.Forms.TextBox goodServiceBox;
+        private System.Windows.Forms.TextBox excellentServiceBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CS-1400/Project 1/Project_01_KJ_V1/Project_01_KJ_V1/Form1.cs
@@ -0,0 +1,79 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: project 1
+//Date: 09/06/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_01_KJ_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //purpose: Outputs information about creator
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kurt Jones\n Project 1\n CS1400_01");
+        }
+
+        //purpose: Terminates program
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //purpose: Clear All Text Boxes
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            costOfMealBox.Clear();
+            poorServiceBox.Clear();
+            goodServiceBox.Clear();
+            excellentServiceBox.Clear();
+        }
+        //purpose: Calculate tip from 3 types of service
+
+        private void costOfMealBox_Leave(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // create constant for poor, good and excellent service
+            const double POOR = .10;
+            const double GOOD = .15;
+            const double EXCELLENT = .20;
+            // take amount of meal and convert to double
+            double totalMeal = double.Parse(costOfMealBox.Text);
+            // take amount of meal and devide by poor service
+            double tipPoor = totalMeal * POOR;
+            string outTipPoor = $"{tipPoor:c}";
+            poorServiceBox.Text = outTipPoor;
+            // take amount of meal and devide by good service
+            double tipGood = totalMeal * GOOD;
+            string outTipGood = $"{tipGood:c}";
+            goodServiceBox.Text = outTipGood;
+            // take amount of meal and devide by excellent service
+            double tipExcellent = totalMeal * EXCELLENT;
+            string outTipExcellent = $"{tipExcellent:c}";
+            excellentServiceBox.Text = outTipExcellent;
+        }
+    }
+}
