debbd7537ad2bda378008b1cb68b9a95b96d25cc
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 18:19:08 -0600

    proj4

---
+++ CS-1400/project 4/Project_4_KJ_V1/Project_4_KJ_V1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Project_4_KJ_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Calc_Btn = new System.Windows.Forms.Button();
+            this.Clear_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sideATxtBx = new System.Windows.Forms.TextBox();
+            this.sideBTxtBx = new System.Windows.Forms.TextBox();
+            this.angleCTxtBx = new System.Windows.Forms.TextBox();
+            this.sideCTxtBx = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.angleATxtBx = new System.Windows.Forms.TextBox();
+            this.AngleBTxtBx = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Calc_Btn
+            // 
+            this.Calc_Btn.Location = new System.Drawing.Point(172, 183);
+            this.Calc_Btn.Name = "Calc_Btn";
+            this.Calc_Btn.Size = new System.Drawing.Size(109, 44);
+            this.Calc_Btn.TabIndex = 0;
+            this.Calc_Btn.Text = "Calculate";
+            this.Calc_Btn.UseVisualStyleBackColor = true;
+            this.Calc_Btn.Click += new System.EventHandler(this.Calc_Btn_Click);
+            // 
+            // Clear_Btn
+            // 
+            this.Clear_Btn.Location = new System.Drawing.Point(172, 289);
+            this.Clear_Btn.Name = "Clear_Btn";
+            this.Clear_Btn.Size = new System.Drawing.Size(109, 47);
+            this.Clear_Btn.TabIndex = 1;
+            this.Clear_Btn.Text = "Clear";
+            this.Clear_Btn.UseVisualStyleBackColor = true;
+            this.Clear_Btn.Click += new System.EventHandler(this.Clear_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Side a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Side b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Angle C (In Degrees)";
+            // 
+            // sideATxtBx
+            // 
+            this.sideATxtBx.Location = new System.Drawing.Point(181, 38);
+            this.sideATxtBx.Name = "sideATxtBx";
+            this.sideATxtBx.Size = new System.Drawing.Size(100, 26);
+            this.sideATxtBx.TabIndex = 5;
+            // 
+            // sideBTxtBx
+            // 
+            this.sideBTxtBx.Location = new System.Drawing.Point(181, 80);
+            this.sideBTxtBx.Name = "sideBTxtBx";
+            this.sideBTxtBx.Size = new System.Drawing.Size(100, 26);
+            this.sideBTxtBx.TabIndex = 6;
+            // 
+            // angleCTxtBx
+            // 
+            this.angleCTxtBx.Location = new System.Drawing.Point(181, 117);
+            this.angleCTxtBx.Name = "angleCTxtBx";
+            this.angleCTxtBx.Size = new System.Drawing.Size(100, 26);
+            this.angleCTxtBx.TabIndex = 7;
+            // 
+            // sideCTxtBx
+            // 
+            this.sideCTxtBx.Location = new System.Drawing.Point(172, 246);
+            this.sideCTxtBx.Name = "sideCTxtBx";
+            this.sideCTxtBx.Size = new System.Drawing.Size(109, 26);
+            this.sideCTxtBx.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Side c:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(302, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(428, 268);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 33);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(51, 29);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(147, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(147, 30);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(298, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "BONUS:";
+            // 
+            // angleATxtBx
+            // 
+            this.angleATxtBx.Location = new System.Drawing.Point(449, 316);
+            this.angleATxtBx.Name = "angleATxtBx";
+            this.angleATxtBx.Size = new System.Drawing.Size(100, 26);
+            this.angleATxtBx.TabIndex = 13;
+            // 
+            // AngleBTxtBx
+            // 
+            this.AngleBTxtBx.Location = new System.Drawing.Point(630, 316);
+            this.AngleBTxtBx.Name = "AngleBTxtBx";
+            this.AngleBTxtBx.Size = new System.Drawing.Size(100, 26);
+            this.AngleBTxtBx.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(374, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Angle A:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(555, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Angle B:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 359);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.AngleBTxtBx);
+            this.Controls.Add(this.angleATxtBx);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sideCTxtBx);
+            this.Controls.Add(this.angleCTxtBx);
+            this.Controls.Add(this.sideBTxtBx);
+            this.Controls.Add(this.sideATxtBx);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clear_Btn);
+            this.Controls.Add(this.Calc_Btn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Project_4_KJ_V1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calc_Btn;
+        private System.Windows.Forms.Button Clear_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sideATxtBx;
+        private System.Windows.Forms.TextBox sideBTxtBx;
+        private System.Windows.Forms.TextBox angleCTxtBx;
+        private System.Windows.Forms.TextBox sideCTxtBx;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox angleATxtBx;
+        private System.Windows.Forms.TextBox AngleBTxtBx;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CS-1400/project 4/Project_4_KJ_V1/Project_4_KJ_V1/Form1.cs
@@ -0,0 +1,114 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Project 4
+//Date: 09/25/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_4_KJ_V1
+{
+    public partial class Form1 : Form
+    {
+        public const int TWO = 2;
+        public const int RADIAN = 180;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The AboutToolStrip Method
+        // Purpose: Displays information about the creator
+        // Parameters: None
+        // Returns: Text
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kurt Jones\n Project 4\n CS1400_01");
+        }
+        // The ExitToolStrip Method
+        // Purpose: Closes the aplication
+        // Parameters: None
+        // Returns: None
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // the ClearButton Method
+        //purpose: Clear all text boxes
+        private void Clear_Btn_Click(object sender, EventArgs e)
+        {
+            sideATxtBx.Clear();
+            sideBTxtBx.Clear();
+            sideCTxtBx.Clear();
+            angleCTxtBx.Clear();
+            angleATxtBx.Clear();
+            AngleBTxtBx.Clear();
+        }
+        //the Calc_Btn_Click Method
+        //Purpose: Calculates the Side of a triangle
+        //Paramaters: 3 sides and 3 angles as doubles
+        //Returns: 6 text boxes as strings
+        private void Calc_Btn_Click(object sender, EventArgs e)
+        {
+
+            //Get Input from user for side a
+            int sideA = int.Parse(sideATxtBx.Text);
+            //Get Input from user for side b
+            int sideB = int.Parse(sideBTxtBx.Text);
+            //Get Input from user for Angle C
+            int angleC = int.Parse(angleCTxtBx.Text);
+            //use law of cosign to find side c. Call cosign method
+            double SideC = lawOfCosign(sideA, sideB, angleC);
+            double angleA = lawOfCosignForA(sideA, sideB, SideC);
+            double angleB = lawOfCosignForB(sideA, sideB, SideC);
+            //Format and output
+            string sideCOutText = $"{SideC:f2}";
+            sideCTxtBx.Text = sideCOutText;
+            string angleAOutText = $"{angleA:f2}";
+            angleATxtBx.Text = angleAOutText;
+            string angleBOutText = $"{angleB:f2}";
+            AngleBTxtBx.Text = angleBOutText;
+
+        }
+        //the lawOfCosign Method
+        //Purpose: takes information and calculates cosign
+        //Paramaters: sideA SideB and AngleC as ints
+        //Returns: sideC as double
+        private double lawOfCosign (int sideA, int sideB, int AngleC)
+        {
+            double sideC = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2) - (TWO * (sideA) * (sideB) * Math.Cos(AngleC * (Math.PI / RADIAN))));
+            return sideC;
+        }
+        //the lawOfCosignForA Method
+        //Purpose: takes information and calculates cosign
+        //Paramaters: sideA SideB and SideC as ints
+        //Returns: AngleA as double
+        private double lawOfCosignForA (int sideA, int sideB, double sideC)
+        {
+            double angleA = Math.Acos((Math.Pow(sideB, 2) + Math.Pow(sideC, 2) - Math.Pow(sideA, 2)) / (TWO * (sideB) * (sideC))) * (RADIAN / Math.PI);
+            return angleA;
+       }
+        //the lawOfCosignForB Method
+        //Purpose: takes information and calculates cosign
+        //Paramaters: sideA SideB and SideC as ints
+        //Returns: AngleB as double
+        private double lawOfCosignForB(int sideA, int sideB, double sideC)
+        {
+            double angleB = Math.Acos((Math.Pow(sideC, 2) + Math.Pow(sideA, 2) - Math.Pow(sideB, 2)) / (TWO * (sideA) * (sideC))) * (RADIAN / Math.PI);
+           return angleB;
+       }
+    }
+}
