ee1500b1b88e26169ee9c33561f741d36f5ec56f
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 18:03:20 -0600

    lab11

---
+++ CS-1400/Lab 11/Lab_11_KJ_V1/Lab_11_KJ_V1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Lab_11_KJ_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sideATxtBox = new System.Windows.Forms.TextBox();
+            this.sideBTxtBox = new System.Windows.Forms.TextBox();
+            this.sideCTxtBox = new System.Windows.Forms.TextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(16, 149);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(90, 46);
+            this.CalculateButton.TabIndex = 0;
+            this.CalculateButton.Text = "Calculate!";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Side A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Side B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Side C:";
+            // 
+            // sideATxtBox
+            // 
+            this.sideATxtBox.Location = new System.Drawing.Point(78, 50);
+            this.sideATxtBox.Name = "sideATxtBox";
+            this.sideATxtBox.Size = new System.Drawing.Size(98, 26);
+            this.sideATxtBox.TabIndex = 4;
+            // 
+            // sideBTxtBox
+            // 
+            this.sideBTxtBox.Location = new System.Drawing.Point(78, 94);
+            this.sideBTxtBox.Name = "sideBTxtBox";
+            this.sideBTxtBox.Size = new System.Drawing.Size(100, 26);
+            this.sideBTxtBox.TabIndex = 5;
+            // 
+            // sideCTxtBox
+            // 
+            this.sideCTxtBox.Location = new System.Drawing.Point(78, 217);
+            this.sideCTxtBox.Name = "sideCTxtBox";
+            this.sideCTxtBox.Size = new System.Drawing.Size(100, 26);
+            this.sideCTxtBox.TabIndex = 6;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(112, 149);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(103, 46);
+            this.ClearButton.TabIndex = 7;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(232, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 285);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(597, 33);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(51, 29);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(211, 30);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 342);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.sideCTxtBox);
+            this.Controls.Add(this.sideBTxtBox);
+            this.Controls.Add(this.sideATxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lab_11_KJ_V1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sideATxtBox;
+        private System.Windows.Forms.TextBox sideBTxtBox;
+        private System.Windows.Forms.TextBox sideCTxtBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+    }
+}
+
---
+++ CS-1400/Lab 11/Lab_11_KJ_V1/Lab_11_KJ_V1/Form1.cs
@@ -0,0 +1,94 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Lab 11
+//Date: 09/17/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_11_KJ_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // the ClearButton Method
+        //purpose: Clear all text boxes
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            sideATxtBox.Clear();
+            sideBTxtBox.Clear();
+            sideCTxtBox.Clear();
+        }
+        // The CalcHypotenuse Method
+        // Purpose: Takes data from the user and calculates the hypotenuse of a triangle
+        // Parameters: two sides of a triangle as doubles
+        // Returns: two sides of a triangle as doubles
+        static double CalcHypotenuse(double side1, double side2)
+        {
+            // take side1 and Square it
+            double side1SQ = Math.Pow(side1, 2);
+            // save to side1SQ
+            // take side 2 and square it
+            double side2SQ = Math.Pow(side2, 2);
+            // save to side1SQ
+            // add together side1sq and side2sq
+            double answerSQ = side1SQ + side2SQ;
+            // save to answer
+            // square root answer
+            double total = Math.Sqrt(answerSQ);
+            // return value 
+            return total;
+        }
+
+        // The ExitToolStrip Method
+        // Purpose: Closes the aplication
+        // Parameters: None
+        // Returns: None
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // The AboutToolStrip Method
+        // Purpose: Displays information about the creator
+        // Parameters: None
+        // Returns: Text
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kurt Jones\nLab 11\nCS1400_01");
+        }
+        // The CalculateButton Method
+        // Purpose: runs the aplication
+        // Parameters: None
+        // Returns: Side C of the triangle
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            //ask for side1 from user
+            double side1 = double.Parse(sideATxtBox.Text);
+            //ask for side2 from user
+            double side2 = double.Parse(sideBTxtBox.Text);
+            //run method CalcHypotenuse
+            double total = CalcHypotenuse(side1, side2);
+            //convert double to string
+            string outPutTotal = $"{total:F2}";
+            //output total
+            sideCTxtBox.Text = outPutTotal;
+        }
+    }
+}
