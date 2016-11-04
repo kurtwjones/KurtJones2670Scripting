4d0b88cd4edd93446293b9e5cd5175c22e47a2c9
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 18:11:19 -0600

    lab19

---
+++ CS-1400/Lab 19/Lab_19_KJ_V1/Lab_19_KJ_V1/Program.cs
@@ -0,0 +1,70 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Lab 19
+//Date: 10/20/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_19_KJ_V1
+{
+    
+    class Program
+    {
+        public RandomNumber Rn;
+        // The Main Method
+        // Purpose: askes the user if they want to play a dice game
+        // Parameters: none
+        // Returns: none
+
+        static void Main(string[] args)
+        {
+            
+            const string YES = "y";
+            const string NO = "n";
+            string response = "";
+            RandomNumber Rn = new RandomNumber();
+            //Ask the user if they want to play
+            do
+            {
+                Console.WriteLine("Hello. Would you like to play a Dice Game (y/n)? ");
+                response = Console.ReadLine();
+                //keep user from putting in unexpected value
+                if (response != YES && response != NO)
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is not a valid response. Please try again.");
+                    
+                }
+               
+            } while (response != YES && response != NO);
+
+            //Get users response. If 'y' call random number program
+            if (response == YES)
+            {
+                Console.Clear();
+                Rn.NumGenerator();
+            }
+            //if 'n' print goodbye message and quit.
+            else
+
+            {
+                Console.Clear();
+                Console.WriteLine("Thanks for playing. Goodbye");
+                Console.Read();
+            }
+
+        }
+    }
+}
---
+++ CS-1400/Lab 19/Lab_19_KJ_V1/Lab_19_KJ_V1/RandomNumber.cs
@@ -0,0 +1,102 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Lab 19
+//Date: 10/20/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_19_KJ_V1
+{
+    class RandomNumber
+    {
+        private const int boxCar = 6;
+        private const int snakeEye = 1;
+        private int firstNumber = 0;
+        private int secondNumber = 0;
+        private const string YES = "y";
+        private const string NO = "n";
+        private string response = "";
+       
+        // The NumGenerator Method
+        // Purpose: Generates two random numbers
+        // Parameters: none
+        // Returns: none
+
+        public void NumGenerator()
+        {
+            //Generate two random numbers between 1-6
+            Random dieValue = new Random();
+            firstNumber = dieValue.Next(1, 7);
+            secondNumber = dieValue.Next(1, 7);
+            //run diceresults method
+            DiceResults();
+
+        }
+        // The DiceResults Method
+        // Purpose: Prints the dice results to console
+        // Parameters: none
+        // Returns: none
+
+        public void DiceResults()
+        {
+            //if the two numbers are 6 and 6 display "You got Boxcars"
+            if (firstNumber == boxCar && secondNumber == boxCar)
+                Console.WriteLine("You got Boxcars");
+            //if the two numbers are 1 and 1 display "You rolled snake-eyes"
+            else if (firstNumber == snakeEye && secondNumber == snakeEye)
+                Console.WriteLine("You rolled snake-eyes.");
+            //if something else display "you rolled" and the two numbers
+            else
+                Console.WriteLine("You rolled {0} and {1}", firstNumber, secondNumber);
+            //run playagain method
+            PlayAgain();
+        }
+        // The PlayAgain Method
+        // Purpose: Askes the user if they want to play again
+        // Parameters: none
+        // Returns: none
+
+        public void PlayAgain()
+        {
+            //Ask the user if they want to play again
+            do
+            {
+                Console.WriteLine("Would you like to play again? (y/n) ");
+                response = Console.ReadLine();
+                Console.Clear();
+                //Keep the user from putting in unexpected values
+                if (response != YES && response != NO)
+                {
+                    Console.WriteLine("That is not a valid response. Please try again.");
+                }
+
+            } while (response != YES && response != NO);
+
+            //Get users response. If 'y' call random number program
+            if (response == YES)
+            {
+                NumGenerator();
+            }
+            //if 'n' print goodbye message and quit.
+            else
+
+            {
+                Console.WriteLine("Thanks for playing. Goodbye");
+                Console.Read();
+            }
+        }
+    }
+}
+
