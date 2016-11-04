c76e919badfe9e4c7508238712f9ca6874cdbf08
kurt jones <kurtwilliamjones@gmail.com>
2016-11-03 18:17:13 -0600

    project3

---
+++ CS-1400/Project 3/Project_03_KJ_V1/Project_03_KJ_V1/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Project_03_KJ_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.oldStartTimeTxtBox = new System.Windows.Forms.TextBox();
+            this.oldEndTimeTxtBox = new System.Windows.Forms.TextBox();
+            this.newEndTimeTxtBox = new System.Windows.Forms.TextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(37, 137);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(112, 78);
+            this.CalculateButton.TabIndex = 0;
+            this.CalculateButton.Text = "GO !";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Old Start Time (HHMM) :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Old End Time (HHMM):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "New End Time (HHMM):";
+            // 
+            // oldStartTimeTxtBox
+            // 
+            this.oldStartTimeTxtBox.Location = new System.Drawing.Point(211, 56);
+            this.oldStartTimeTxtBox.Name = "oldStartTimeTxtBox";
+            this.oldStartTimeTxtBox.Size = new System.Drawing.Size(119, 26);
+            this.oldStartTimeTxtBox.TabIndex = 4;
+            // 
+            // oldEndTimeTxtBox
+            // 
+            this.oldEndTimeTxtBox.Location = new System.Drawing.Point(211, 93);
+            this.oldEndTimeTxtBox.Name = "oldEndTimeTxtBox";
+            this.oldEndTimeTxtBox.Size = new System.Drawing.Size(119, 26);
+            this.oldEndTimeTxtBox.TabIndex = 5;
+            // 
+            // newEndTimeTxtBox
+            // 
+            this.newEndTimeTxtBox.Location = new System.Drawing.Point(211, 235);
+            this.newEndTimeTxtBox.Name = "newEndTimeTxtBox";
+            this.newEndTimeTxtBox.Size = new System.Drawing.Size(119, 26);
+            this.newEndTimeTxtBox.TabIndex = 6;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(161, 137);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(119, 78);
+            this.ClearButton.TabIndex = 7;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 33);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(51, 29);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(147, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(147, 30);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 298);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.newEndTimeTxtBox);
+            this.Controls.Add(this.oldEndTimeTxtBox);
+            this.Controls.Add(this.oldStartTimeTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Project_03_KJ_V1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox oldStartTimeTxtBox;
+        private System.Windows.Forms.TextBox oldEndTimeTxtBox;
+        private System.Windows.Forms.TextBox newEndTimeTxtBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+    }
+}
+
---
+++ CS-1400/Project 3/Project_03_KJ_V1/Project_03_KJ_V1/Form1.cs
@@ -0,0 +1,105 @@
+﻿//Project Prolog
+//Name: Kurt Jones
+//CS 1400 Section 001
+//Project: Project 3
+//Date: 09/17/16
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+//---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_03_KJ_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // The AboutToolStrip Method
+        // Purpose: Displays information about the creator
+        // Parameters: None
+        // Returns: Text
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kurt Jones\nProject 3\nCS1400_01");
+        }
+        // The ExitToolStrip Method
+        // Purpose: Closes the aplication
+        // Parameters: None
+        // Returns: None
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // the ClearButton Method
+        //purpose: Clear all text boxes
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            oldStartTimeTxtBox.Clear();
+            oldEndTimeTxtBox.Clear();
+            newEndTimeTxtBox.Clear();    
+        }
+        //the CalculateButton Method
+        //Purpose: Calculates the new time after construction
+        //Paramaters: two ints which contain the old start and end times
+        //Returns: an int that contains the new end time
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            //declare constants
+            const int MINUTES = 60;
+            const int FULLDAY = 1440;
+            const double CONSTRUCTION = .25;
+            const int HUNDRED_PART = 100;
+            //Get old start time in 24hr format
+            int oldStartTime = int.Parse(oldStartTimeTxtBox.Text);
+            //Get old end time in 24hm format
+            int oldEndTime = int.Parse(oldEndTimeTxtBox.Text);
+            //set values and convert to minutes
+            int oldStartTimeHours = oldStartTime / HUNDRED_PART;
+            int oldStartTimeminutes = oldStartTime % HUNDRED_PART;
+            int oldEndTimeHours = oldEndTime / HUNDRED_PART;
+            int oldEndTimeminutes = oldEndTime % HUNDRED_PART;
+            int minOldStart = MINUTES * oldStartTimeHours + oldStartTimeminutes;
+            int minOldEnd = MINUTES * oldEndTimeHours + oldEndTimeminutes;
+            //Subtract start time to end time
+            int minNewEnd = minOldEnd - minOldStart;
+            //some logic to keep it right just in case it rolls over
+            if (minOldEnd < minOldStart)
+            {
+                minNewEnd = minNewEnd + FULLDAY;
+            }
+
+            //add 25 percent time for construction
+            double constructionDelay = minNewEnd * CONSTRUCTION;
+            int newMinEndTime = ((int)constructionDelay) + minOldEnd;
+            //logic in case you have a huge drive time
+            if (newMinEndTime > FULLDAY)
+            {
+               newMinEndTime = newMinEndTime - FULLDAY;
+            }
+
+            //convert back to HHMM
+            int newEndHours = newMinEndTime / MINUTES;
+            int newEndMinutes = (int)(MINUTES * (((double)newMinEndTime / MINUTES) - newEndHours));
+
+            //display new end time
+            string outNewEndTime = $"{newEndHours:d2}" + $"{newEndMinutes:d2}";
+            newEndTimeTxtBox.Text = outNewEndTime;
+            
+        }
+    }
+}
