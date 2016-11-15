dda6372eb0de09096b93df6aff559486034902e5
kurt jones <kurtwilliamjones@gmail.com>
2016-11-15 14:54:55 -0700

    added movement

---
+++ FinalGame/Final Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float speed = 1;
+    private CharacterController cc;
+    public float gravity = -9.8f;
+
+    private Vector3 tempPosition;
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempPosition.y = gravity;
+        tempPosition.x = speed * Input.GetAxis("Horizontal");
+        tempPosition *= Time.deltaTime;
+        cc.Move(tempPosition);
+	}
+}
