79a01e291bd4091ff315ebb9c0cd8b0bb88dfc48
kurt jones <kurtwilliamjones@gmail.com>
2016-11-30 16:42:29 -0700

    test

---
+++ FinalGame/Final Game/Assets/Scripts/CameraLookAtPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLookAtPlayer : MonoBehaviour {
+
+    public Transform player;
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+	}
+}
