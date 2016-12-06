defd3d31dd91c408f7129a0927ee91b056f62070
kurt jones <kurtwilliamjones@gmail.com>
2016-12-06 16:22:45 -0700

    score counter: delegate

--- FinalGame/Final Game/Assets/Scripts/Pick_Up.cs
+++ FinalGame/Final Game/Assets/Scripts/Pick_Up.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Pick_Up : MonoBehaviour {
 
     public int counter = 0;
-    public Transform pick_up;
-	// Use this for initialization
+    private const int MAX = 10;
+    public static Action<string> scoreToPass;
+
 	void OnTriggerEnter()
     {
-        transform.position = new Vector3(Random.Range(-10,10), 1, 0);
+        transform.position = new Vector3(UnityEngine.Random.Range(-MAX,MAX), 1, 0);
         counter++;
+        PassScore();
+       
+    }
+    private void PassScore()
+    {
+        scoreToPass(counter.ToString());
     }
 }
---
+++ FinalGame/Final Game/Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class Score : MonoBehaviour {
+    string counter = "0";
+    public Text scoreCount;
+    
+	// Use this for initialization
+	void Start () {
+        Pick_Up.scoreToPass += scoreHandler;
+        counter = "0";
+        scoreCount.text = "Score: " + counter;
+	}
+
+    private void scoreHandler(string score)
+    {
+       counter = score;
+       scoreCount.text = "Score: " + counter;
+    }
+}
